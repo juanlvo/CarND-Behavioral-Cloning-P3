712f34e465d4a80eec5bafe30975eb74a7e24d41
Juan Vivas <Juan Vivas>
2017-04-17 21:19:28 +0200

    clenning the code

--- model.py
+++ model.py
@@ -10,59 +10,7 @@ with open('data/driving_log.csv') as csvfile:
         lines.append(line)		
 
 images = []
-measurements = []
-	
-# for line in lines:
-
-	# # center camera
-    # source_path = line[0]
-    # filename = source_path.split('/')[-1]
-    # current_path = 'data/IMG/' + filename
-    # #print(current_path)
-    # image_center = cv2.imread(current_path)
-	
-	# # left camera
-    # source_path = line[1]
-    # filename = source_path.split('/')[-1]
-    # current_path = 'data/IMG/' + filename
-    # image_left = cv2.imread(current_path)	
-	
-	# # right camera
-    # source_path = line[2]
-    # filename = source_path.split('/')[-1]
-    # current_path = 'data/IMG/' + filename
-    # image_right = cv2.imread(current_path)	
-		
-    # correction = float(0.2)		
-		
-	# # original center camera
-    # measurement_center = float(line[3])
-	
-	# # original left camera
-    # measurement_left = float(line[3]) + correction	
-	
-	# # original right camera
-    # measurement_right = float(line[3]) - correction	
-
-	# # add images and angles to data set
-    # images.extend((image_center, image_left, image_right))
-    # measurements.extend((measurement_center, measurement_left, measurement_right))	
-
-	# # inverse center camera
-    # images_inverse_center = cv2.flip(image_center, 1)
-    # measurements_inverse_center = measurement_center*-1.0  
-
-	# # inverse left camera
-    # images_inverse_left = cv2.flip(image_left, 1)
-    # measurements_inverse_left = measurement_left*-1.0	
-	
-	# # inverse right camera
-    # images_inverse_right = cv2.flip(image_right, 1)
-    # measurements_inverse_right = measurement_right*-1.0	
-	
-	# # add images and angles to data set
-    # images.extend((images_inverse_center, images_inverse_left, images_inverse_right))
-    # measurements.extend((measurements_inverse_center, measurements_inverse_left, measurements_inverse_right))		
+measurements = []	
 
 from sklearn.model_selection import train_test_split
 train_samples, validation_samples = train_test_split(lines, test_size=0.3)
@@ -83,6 +31,7 @@ def generator(samples, batch_size=64):
       measurements = []
       for batch_sample in batch_samples:
         
+		#center images
         name_center = 'data/IMG/'+batch_sample[0].split('/')[-1]
         image_center = cv2.imread(name_center)
         measurement_center = float(batch_sample[3])
@@ -90,6 +39,7 @@ def generator(samples, batch_size=64):
         images.append(image_center)
         measurements.append(measurement_center)
 		
+		#left images
         name_left = 'data/IMG/'+batch_sample[1].split('/')[-1]
         image_left = cv2.imread(name_left)
         measurement_left = float(batch_sample[3]) + correction
@@ -97,6 +47,7 @@ def generator(samples, batch_size=64):
         images.append(image_left)
         measurements.append(measurement_left)
         
+		#right images
         name_right = 'data/IMG/'+batch_sample[2].split('/')[-1]
         image_right = cv2.imread(name_right)
         measurement_right = float(batch_sample[3]) - correction				
@@ -125,7 +76,6 @@ def generator(samples, batch_size=64):
         images.append(images_inverse_right)
         measurements.append(measurements_inverse_right)							
 
-      # trim image to only see section with road
       X_train = np.array(images)
       y_train = np.array(measurements)
       yield sklearn.utils.shuffle(X_train, y_train)
@@ -144,12 +94,13 @@ from keras.layers.core import Dropout
 from keras.layers.advanced_activations import ELU
 import matplotlib.pyplot as plt
 
-#ch, row, col = 3, 80, 320  # Trimmed image format
-#ch, row, col = 66, 200, 3
+#########################################################################
+#
+# Model NVIDEA
+#
+#########################################################################
 
 model = Sequential()
-#model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(ch, row, col), output_shape=(ch, row, col)))
-#model.add(Lambda(lambda x: x/127.5 - 1., input_shape=(ch, row, col),output_shape=(ch, row, col)))
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
 model.add(Cropping2D(cropping=((65,25), (0,0))))
 model.add(Convolution2D(24,5,5,subsample=(2,2),activation="relu"))
@@ -174,7 +125,6 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-#history_object = model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=6, verbose=1)
 history_object = model.fit_generator(train_generator, samples_per_epoch=len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=30, verbose=1)
 
 model.save('model.h5')
